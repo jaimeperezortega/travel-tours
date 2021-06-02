c491e798f5a19fda65294697a7e06d28d77401a2
jaimeperezortega <jaimeperezortegadev@gmail.com>
2021-06-02 17:31:41 +0200

    Finished project

--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+
+  const eraseTour = (id) => {
+    console.log('Has hecho click en el tour con id:', id);
+  }
+
+  return (
+    <section>
+      <div className= "title">
+        <h2>Our tours</h2>
+        <div className="underline"></div>
+        <div>
+         {tours.map(tour => {
+           //const {name, info, image, price } = tours;
+           return <Tour removeTour={removeTour}  key = {tour.id} {...tour}/>
+         })}
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
